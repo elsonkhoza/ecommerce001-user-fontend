cb1c49deb5b16763f1a254ed589b9cdaecb4b601
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-10-21 10:26:30 +0200

    updated product actions and reducers

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -21,3 +21,4 @@ export const emptySelectedProduct=()=>{
         payload:{}
     }
 }
+
--- src/redux/reducers/productsReducer.js
+++ src/redux/reducers/productsReducer.js
@@ -1,9 +1,19 @@
-import { SET_PRODUCTS } from "../actions/types";
+import { EMPTY_SELECTED_PRODUCT, REMOVE_FROM_CART, SELECT_PRODUCT, SET_PRODUCTS } from "../actions/types";
 
 export const productsReducer=(state=[],{type,payload})=>{
 
     switch(type){
         case SET_PRODUCTS: return [...state,payload];
         default : return state;
+    }
+}
+
+export const selectedProductReducer=(state={},{type,payload})=>{
+
+    switch(type)
+    {
+        case SELECT_PRODUCT : return {...state,...payload};
+        case EMPTY_SELECTED_PRODUCT: return {};
+        default: return state;
     }
 }
